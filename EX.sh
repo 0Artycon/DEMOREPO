f3fd6f510f18ad0b432bf3dc747cd3d474bf918c
0Artycon <123401816+0Artycon@users.noreply.github.com>
2024-05-15 13:35:50 +0500

    Update EX.sh

--- EX.sh
+++ EX.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-qm create 100 --name "ISP" --cores 2 --memory 2048 --ostype l26 --scsi0 tomb:30  --scsihw virtio-scsi-single  --net0 virtio,bridge=vmbr0 --net1 virtio,bridge=vmbr1 --net2 virtio,bridge=vmbr2
+qm create 100 --name "ISP" --cores 2 --memory 2048 --ostype l26 --scsi0 tomb:30 --cdrom local:iso/AltLinux-Server.iso --scsihw virtio-scsi-single --net0 virtio,bridge=vmbr0 --net1 virtio,bridge=vmbr1 --net2 virtio,bridge=vmbr2
+qm create 101 --name "HQ-R" --cores 2 --memory 2048 --ostype l26 --scsi0 tomb:30 --cdrom local:iso/AltLinux-Server.iso --scsihw virtio-scsi-single --net0 virtio,bridge=vmbr0 --net1 virtio,bridge=vmbr1 --net2 virtio,bridge=vmbr2
+qm create 102 --name "BR-R" --cores 2 --memory 2048 --ostype l26 --scsi0 tomb:30 --cdrom local:iso/vesr-1.18.9-build3.iso --scsihw virtio-scsi-single --net0 virtio,bridge=vmbr0 --net1 virtio,bridge=vmbr1 --net2 virtio,bridge=vmbr2
+qm create 103 --name "HQ-SRV" --cores 2 --memory 2048 --ostype l26 --scsi0 tomb:30 --cdrom local:iso/AltLinux-Server.iso --scsihw virtio-scsi-single --net0 virtio,bridge=vmbr0 --net1 virtio,bridge=vmbr1 --net2 virtio,bridge=vmbr2
+qm create 104 --name "BR-SRV" --cores 2 --memory 2048 --ostype l26 --scsi0 tomb:30 --cdrom local:iso/AltLinux-Server.iso --scsihw virtio-scsi-single --net0 virtio,bridge=vmbr0 --net1 virtio,bridge=vmbr1 --net2 virtio,bridge=vmbr2
+qm create 105 --name "CLI" --cores 2 --memory 3072 --ostype l26 --scsi0 tomb:30 --cdrom local:iso/alt-workstation-10.1-x86_64.iso --scsihw virtio-scsi-single --net0 virtio,bridge=vmbr0 --net1 virtio,bridge=vmbr1 --net2 virtio,bridge=vmbr2
